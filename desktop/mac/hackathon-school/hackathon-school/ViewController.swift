66b4fbb86352fbea9ed971bfc4704e7f866de7ce
AplinXy9plin <evilberry@icloud.com>
2017-11-04 17:00:28 +0700

    continue

---
+++ desktop/mac/hackathon-school/hackathon-school/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  hackathon-school
+//
+//  Created by Никита Аплин on 04.11.2017.
+//  Copyright © 2017 Никита Аплин. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+    
+}
+
